5c8bf9304d5b80c88b461543d4c3a633af77f8c3
tiberius-s <tiberius220@gmail.com>
2017-07-29 12:44:17 -0500

    Formatting

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,5 @@
-const routes = require('express').Router();
-const request = require('routes/calls');
+const routes = require("express").Router();
+const request = require("routes/calls");
 const headers = require("middleware/headers");
 const options = require("middleware/options");
 
@@ -8,21 +8,18 @@ routes.use(headers);
 routes.use(options);
 
 // administration
-routes.get('/administration/users/current', request.send(200));
+routes.get("/administration/users/current", request.send(200));
 
 // lists
-routes.get('/lists/:acct', request.send(200));
-routes.post('/lists/:acct', request.send(201));
-routes.get('/lists/:acct/:list', request.send(200));
-routes.put('/lists/:acct/:list', request.send(204));
-routes.delete('/lists/:acct/:list', request.send(204));
-
-
+routes.get("/lists/:acct", request.send(200));
+routes.post("/lists/:acct", request.send(201));
+routes.get("/lists/:acct/:list", request.send(200));
+routes.put("/lists/:acct/:list", request.send(204));
+routes.delete("/lists/:acct/:list", request.send(204));
 
 // For testing
-routes.get('/', (req, res) => {
-  res.status(200).json({ msg: 'Hello World!' });
-})
-
+routes.get("/", (req, res) => {
+  res.status(200).json({ msg: "Hello World!" });
+});
 
 module.exports = routes;
