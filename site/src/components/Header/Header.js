a4c15d137eecc8d7a2a7e1a407a834aed628c89d
tiberius-s <tiberius220@gmail.com>
2017-07-30 10:38:59 -0500

    added some basic styling and elements, specifically the header component

---
+++ site/src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-title">rmjs</div>
+      <div className="header-content">
+        <p>Demo on some of the basic interactions with the ReachMail API. </p>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
