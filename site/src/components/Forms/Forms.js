d45cfb52344dac98af887e0317115b424376f5f1
tiberius-s <tiberius220@gmail.com>
2017-07-30 21:08:38 -0500

    reformated Forms to stateless component

--- site/src/components/Forms/Forms.js
+++ site/src/components/Forms/Forms.js
@@ -1,29 +1,22 @@
-import React, { Component } from "react";
+import React from "react";
 import CreateList from "./CreateList";
 import CreateRecipient from "./CreateRecipient";
 import CreateMailing from "./CreateMailing";
 import ScheduleCampaign from "./ScheduleCampaign";
 
-class Forms extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { ...props };
-  }
-
-  render() {
-    const helpers = this.state;
-    return (
-      <div className="view">
-        <div className="view-header">Forms</div>
-        <div className="view-content">
-          <CreateList {...helpers} />
-          <CreateRecipient {...helpers} />
-          <CreateMailing {...helpers} />
-          <ScheduleCampaign {...helpers} />
-        </div>
+const Forms = props => {
+  const helpers = { ...props };
+  return (
+    <div className="view">
+      <div className="view-header">Forms</div>
+      <div className="view-content">
+        <CreateList {...helpers} />
+        <CreateRecipient {...helpers} />
+        <CreateMailing {...helpers} />
+        <ScheduleCampaign {...helpers} />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Forms;
