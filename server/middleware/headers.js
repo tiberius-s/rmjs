16ab3bc0d829314ebd50ce28d09c362c8030b491
tiberius-s <tiberius220@gmail.com>
2017-07-31 00:36:56 -0500

    added no-cache to headers on server

--- server/middleware/headers.js
+++ server/middleware/headers.js
@@ -1,10 +1,12 @@
 module.exports = (req, res, next) => {
   res.set({
-    'Accept': 'application/json',
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
-    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
-    'Content-Type': "application/json"
+    Accept: "application/json",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
+    "Access-Control-Allow-Headers":
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization",
+    "Cache-Control": "no-cache",
+    "Content-Type": "application/json"
   });
   next();
 };
