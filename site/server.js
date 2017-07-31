aaa388253e838fdfb6db47d351e3c2116ea9d80f
tiberius-s <tiberius220@gmail.com>
2017-07-30 22:49:23 -0500

    comments in server

--- site/server.js
+++ site/server.js
@@ -1,8 +1,11 @@
+// Initialize
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 const PORT = 3000;
 
+// serve the built application
 app.use(express.static('build'))
 
+// Server
 server.listen(PORT, () => console.log(`Running on http://localhost:${PORT}`));
